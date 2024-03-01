4ea200b48a5f82dff4cd4f52fabb2bd86e659c8e
schobbejak <jmvanselm@gmail.com>
2024-03-01 11:57:37 +0100

    Full model line coverage

---
+++ tests/pipeline/model/test_model.py
@@ -0,0 +1,19 @@
+import pytest
+from epochalyst.pipeline.model.model import ModelPipeline
+
+
+class TestModel:
+
+    def test_model_pipeline_init(self):
+        model = ModelPipeline()
+        assert model is not None
+
+    def test_model_pipeline_train(self):
+        model = ModelPipeline()
+
+        assert model.train(None, None) == (None, None)
+
+    def test_model_pipeline_predict(self):
+        model = ModelPipeline()
+        
+        assert model.predict(None) == None
