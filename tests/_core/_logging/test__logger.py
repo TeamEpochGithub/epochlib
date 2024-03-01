9789d2c5ee5188185c2783168c61a8ff9ee88914
schobbejak <jmvanselm@gmail.com>
2024-03-01 11:57:23 +0100

    Full _Logger line coverage

---
+++ tests/_core/_logging/test__logger.py
@@ -0,0 +1,34 @@
+import pytest
+from epochalyst._core._logging._logger import _Logger
+
+
+class Test_Logger:
+
+    def test__logger_init(self):
+        _logger = _Logger()
+        assert _logger is not None
+
+    def test__logger_log_to_terminal(self):
+        _logger = _Logger()
+        with pytest.raises(NotImplementedError):
+            assert _logger.log_to_terminal("")
+
+    def test__logger_log_to_debug(self):
+        _logger = _Logger()
+        with pytest.raises(NotImplementedError):
+            assert _logger.log_to_debug("")
+
+    def test__logger_log_to_warning(self):
+        _logger = _Logger()
+        with pytest.raises(NotImplementedError):
+            assert _logger.log_to_warning("")
+
+    def test__logger_log_to_external(self):
+        _logger = _Logger()
+        with pytest.raises(NotImplementedError):
+            assert _logger.log_to_external({})
+
+    def test__logger_external_define_metric(self):
+        _logger = _Logger()
+        with pytest.raises(NotImplementedError):
+            assert _logger.external_define_metric("", "")
