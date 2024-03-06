910b08caca3dbc9b5a5e1b7d33e69c7efaa3d6b7
schobbejak <jmvanselm@gmail.com>
2024-03-06 09:47:09 +0100

    Update training block and class comments

--- epochalyst/_core/_logging/_logger.py
+++ epochalyst/_core/_logging/_logger.py
@@ -5,13 +5,43 @@ from typing import Any
 class _Logger:
     """Logger abstract class for logging methods.
     
-    This class is used to define the logging methods, the following of which should be overridden in the child class:
-    - log_to_terminal(message: str): Log terminal method, if no logging override with empty.
-    - log_to_debug(message: str): Log debug method, if no logging override with empty.
-    - log_to_warning(message: str): Log warning method, if no logging override with empty.
-    - log_to_external(message: dict[str, Any], **kwargs: Any): Log external method, if no logging override with empty.
-    - external_define_metric(metric: str, metric_type: str): Define metric for external. Example: (wandb.define_metric("Training/Train Loss", summary="min"))
+    ### Methods:
+    ```python
+    @abstractmethod
+    def log_to_terminal(self, message: str) -> None: # Logs to terminal if implemented
+
+    @abstractmethod
+    def log_to_debug(self, message: str) -> None: # Logs to debugger if implemented
+    
+    @abstractmethod
+    def log_to_warning(self, message: str) -> None: # Logs to warning if implemented
+
+    @abstractmethod
+    def log_to_external(self, message: dict[str, Any], **kwargs: Any) -> None: # Logs to external site
+    
+    @abstractmethod
+    def external_define_metric(self, metric: str, metric_type: str) -> None: # Defines an external metric
     """
+
+    @abstractmethod
+    def log_to_external(self, message: dict[str, Any], **kwargs: Any) -> None:
+        """Log external method, if no logging override with empty.
+
+        :param message: The message to log."""
+        raise NotImplementedError(
+            f"Log external method not implemented for {self.__class__}"
+        )
+
+    @abstractmethod
+    def external_define_metric(self, metric: str, metric_type: str) -> None:
+        """Define metric for external. Example: (wandb.define_metric("Training/Train Loss", summary="min"))
+
+        :param metric: The metric to define.
+        :param metric_type: The type of the metric."""
+        raise NotImplementedError(
+            f"External define metric method not implemented for {self.__class__}"
+        )
+
 
     @abstractmethod
     def log_to_terminal(self, message: str) -> None:
--- epochalyst/pipeline/model/training/training_block.py
+++ epochalyst/pipeline/model/training/training_block.py
@@ -1,16 +1,65 @@
 from agogos.trainer import Trainer
+from typing import Any
 from epochalyst._core._logging._logger import _Logger
+from abc import abstractmethod
+from epochalyst._core._caching._cacher import _Cacher
 
-
-class TrainingBlock(Trainer, _Logger):
+class TrainingBlock(Trainer, _Cacher, _Logger):
     """The training block is a flexible block that allows for training of any model.
 
-    To use this block, you must inherit from it and implement the following methods:
-    - train(x: Any, y: Any, train_args: dict[str, Any] | None = None) -> tuple[Any, Any]
-    - predict(x: Any, predict_args: dict[str, Any] | None = None) -> Any
-    - log_to_terminal(message: str) -> None
-    - log_to_debug(message: str) -> None
-    - log_to_warning(message: str) -> None
-    - log_to_external(message: dict[str, Any], **kwargs: Any) -> None
-    - external_define_metric(metric: str, metric_type: str) -> None
+    
     """
+
+    def train(self, x: Any, y: Any, cache_args: dict[str, Any] = {}, **kwargs: Any) -> tuple[Any, Any]:
+        """Train the model.
+
+        :param x: The input data.
+        :param y: The target data.
+        :param cache_args: The cache arguments.
+        :return: The predicted data and the labels
+        """
+        if cache_args and self._cache_exists(name=self.get_hash() + "x", cache_args=cache_args) and self._cache_exists(name=self.get_hash() + "y"):
+            x = self._get_cache(name=self.get_hash() + "x", cache_args=cache_args)
+            y = self._get_cache(name=self.get_hash() + "y", cache_args=cache_args)
+        
+        x, y = self.custom_train(x, y, **kwargs)
+
+        self._store_cache(name=self.get_hash() + "x", data=x)
+        self._store_cache(name=self.get_hash() + "y", data=y)
+
+        return x, y
+    
+    @abstractmethod
+    def custom_train(self, x: Any, y: Any) -> tuple[Any, Any]:
+        """Train the model.
+
+        :param x: The input data.
+        :param y: The target data.
+        :return: The predicted data and the labels
+        """
+        raise NotImplementedError(f"Custom transform method not implemented for {self.__class__}")
+
+    def predict(self, x: Any, cache_args: dict[str, Any] = {}, **kwargs: Any) -> Any:
+        """Predict using the model.
+
+        :param x: The input data.
+        :param cache_args: The cache arguments.
+        :return: The predicted data.
+        """
+        if cache_args and self._cache_exists(name=self.get_hash() + "p", cache_args=cache_args):
+            return self._get_cache(name=self.get_hash() + "p", cache_args=cache_args)
+        
+        x = self.custom_predict(x, **kwargs)
+
+        self._store_cache(name=self.get_hash() + "x", data=x)
+
+        return x
+
+    @abstractmethod
+    def custom_predict(self, x: Any) -> Any:
+        """Predict using the model.
+
+        :param x: The input data.
+        :return: The predicted data.
+        """
+        raise NotImplementedError(f"Custom transform method not implemented for {self.__class__}")
