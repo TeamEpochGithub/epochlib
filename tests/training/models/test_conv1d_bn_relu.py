66dd70c28a211cfb8197a3eac237022806d72ca3
schobbejak <jmvanselm@gmail.com>
2024-08-19 22:07:32 +0200

    Improve test coverage and fix spelling mistake for Conv1dBnRelu

--- tests/training/models/test_conv1d_bn_relu.py
+++ tests/training/models/test_conv1d_bn_relu.py
@@ -8,10 +8,16 @@ class TestConv1dBnRelu(TestCase):
 
     conv1d_bn_relu = Conv1dBnRelu(in_channels=3, out_channels=1)
 
-    def test_timm_init(self):
+    def test_conv1d_bn_relu_init(self):
         assert self.conv1d_bn_relu is not None
 
-    def test_conv_1d_bn_relu_forward(self):
+    def test_conv1d_bn_relu_forward(self):
         input = torch.rand(16, 3, 1)
         # Check there is no error thrown
         self.conv1d_bn_relu.forward(input)
+
+    def test_conv1d_bn_relu_forward_without_bn(self):
+        conv1d_relu = Conv1dBnRelu(in_channels=3, out_channels=1, is_bn=False)
+        input = torch.rand(16, 3, 1)
+        # Check there is no error thrown
+        conv1d_relu.forward(input)
